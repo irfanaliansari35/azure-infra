c6f57526a08f89bd47b07744bd0065e45226b531
Irfan Ali Ansari <irfanaliansari35@gmail.com>
2026-01-06 23:58:49 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~> 4"
     }
   }
 }
@@ -10,5 +10,4 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
 }
